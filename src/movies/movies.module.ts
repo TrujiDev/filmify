646e427609f6d234aa28e51f4075e176323860d6
TrujiDev <eetgrisales@gmail.com>
2024-10-27 13:54:42 -0500

    Refactor MoviesModule to integrate TypeORM and export MoviesService
    - Added TypeOrmModule.forFeature to import the Movie entity for repository access.
    - Exported MoviesService to make it available for injection in other modules.

--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { MoviesController } from './movies.controller';
 import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Movie } from './entities/movie.entity';
 
 @Module({
-  imports: [CloudinaryModule],
+  imports: [
+    CloudinaryModule,
+    TypeOrmModule.forFeature([Movie])
+  ],
   controllers: [MoviesController],
   providers: [MoviesService],
+  exports: [MoviesService]
 })
 export class MoviesModule {}

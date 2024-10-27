3ec98c7cc358853cef6cc9d6ad5d317b15ae58d3
TrujiDev <eetgrisales@gmail.com>
2024-10-27 14:10:18 -0500

    feat(main): integrate Swagger for API documentation
    - Configured Swagger with DocumentBuilder for the Filmify API.
    - Added API title, description, version, and a tag for movies.
    - Set up Swagger UI at the '/api' endpoint.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Filmify API')
+    .setDescription('API para gestionar peliculas')
+    .setVersion('1.0')
+    .addTag('movies')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,

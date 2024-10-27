58713f41c6dd4466859623464cd05936c982b96a
TrujiDev <eetgrisales@gmail.com>
2024-10-27 13:55:20 -0500

    Enhance MoviesController to manage movie creation and updates with image uploads
    - Added createMovie endpoint to handle movie creation with file upload.
    - Integrated findAll, getMovieById, updateMovie, and deleteMovie endpoints for movie management.
    - Used CloudinaryService to upload images and store the image URL in the movie entity.
    - Implemented file validation for image uploads with maximum size and allowed file types.

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,24 +1,72 @@
-import { Controller, Get, Post, UseInterceptors, UploadedFile, ParseFilePipe, MaxFileSizeValidator, FileTypeValidator } from '@nestjs/common';
+import { Controller, Get, Post, UseInterceptors, UploadedFile, ParseFilePipe, MaxFileSizeValidator, FileTypeValidator, Body, Param, Patch, Delete, Query } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
+import { MoviesService } from './movies.service';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+import { FilterMoviesDto } from './dto/filter-movies.dto';
+import { Movie } from './entities/movie.entity';
 
 @Controller('movies')
 export class MoviesController {
   constructor(
+    private readonly moviesService: MoviesService,
     private readonly cloudinaryService: CloudinaryService
   ) { }
-  
-  @Post('upload')
+
+  @Post()
   @UseInterceptors(FileInterceptor('file'))
-  async uploadImage(
+  async createMovie(
+    @Body() createMovieDto: CreateMovieDto,
     @UploadedFile(
       new ParseFilePipe({
         validators: [
           new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 4 }),
-          new FileTypeValidator({fileType: '(png|jpeg|jpg)'}),
+          new FileTypeValidator({ fileType: '(png|jpeg|jpg)' }),
+        ],
+      }),
+    ) file: Express.Multer.File,
+  ) {
+    const uploadResult = await this.cloudinaryService.uploadFile(file);
+    createMovieDto.image = uploadResult.secure_url;
+
+    return this.moviesService.create(createMovieDto);
+  }
+
+  @Get()
+  async findAll(@Query() filters: FilterMoviesDto): Promise<Movie[]> {
+    return this.moviesService.findAll(filters);
+  }
+
+  @Get(':id')
+  async getMovieById(@Param('id') id: string) {
+    return this.moviesService.findOne(id);
+  }
+
+  @Patch(':id')
+  @UseInterceptors(FileInterceptor('file'))
+  async updateMovie(
+    @Param('id') id: string,
+    @Body() updateMovieDto: UpdateMovieDto,
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 4 }),
+          new FileTypeValidator({ fileType: '(png|jpeg|jpg)' }),
         ]
       }),
-    ) file: Express.Multer.File) {
-    return this.cloudinaryService.uploadFile(file)
+    ) file: Express.Multer.File
+  ) {
+    if (file) {
+      const uploadResult = await this.cloudinaryService.uploadFile(file);
+      updateMovieDto.image = uploadResult.secure_url;
+    }
+
+    return this.moviesService.update(id, updateMovieDto);
+  }
+
+  @Delete(':id')
+  async deleteMovie(@Param('id') id: string) {
+    return this.moviesService.remove(id);
   }
 }

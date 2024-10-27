e16814225e73ce2a8fec4b68435a305bc9682bee
TrujiDev <eetgrisales@gmail.com>
2024-10-27 12:40:51 -0500

    feat(database): configure TypeORM for PostgreSQL connection with environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { MoviesModule } from './movies/movies.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [MoviesModule, ConfigModule.forRoot()],
+  imports: [
+    MoviesModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PGHOST,
+      username: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.PGDATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      autoLoadEntities: true,
+    })
+  ],
   controllers: [],
   providers: [],
 })
